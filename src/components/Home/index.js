7a3bd79806e6d7ba903325f451077af8bd6975aa
Felipe F Gil <fariagil2018@gmail.com>
2024-08-02 17:05:48 -0500

    Still in progress, stopped at 35:33.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,21 @@
 import LogoTitle from '../../assets/images/logo-s.png';
+import { Link } from 'react-router-dom';
+import './index.scss';
+
 const Home = () => {
+    
 
     return (
-        <div classname="container home-page">
+        <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm</h1>
+                <h1>Hi, <br /> I'm
                 <img src={LogoTitle} alt="developer" />
+                lobodan
+                <br />
+                web developer
+                </h1>
+                <h2>Full-Stack Developer</h2>
+                <Link to="/contact" className='flat-button'>Contact Me</Link>
             </div>
 
         </div>
